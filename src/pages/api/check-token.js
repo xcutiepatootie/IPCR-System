805409bb62c40412ba90689d275ba3e7a1f0b4c2
xcutiepatootie <mackycalabia@gmail.com>
2023-05-30 19:04:53 +0800

    fixed check-token

--- src/pages/api/check-token.js
+++ src/pages/api/check-token.js
@@ -2,12 +2,12 @@ import { verify } from 'jsonwebtoken';
 
 export default async function handler(req, res) {
   const { token } = req.body;
-  
+  console.log(token)
 
   try {
     // Verify the token using your secret or public key
     const decoded = verify(token, process.env.ACCESS_TOKEN);
-    
+    console.log(decoded)
 
     // Token is valid
     res.status(200).json({ message: 'Token is valid' });
