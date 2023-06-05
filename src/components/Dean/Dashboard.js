5e83d874b678dbd792c8854d117baec01a5805f7
stephaniexjoy <cristobalstephaniejoy@gmail.com>
2023-06-06 02:10:04 +0800

    edit dashboard

--- src/components/Dean/Dashboard.js
+++ src/components/Dean/Dashboard.js
@@ -29,10 +29,29 @@ const Dashboard = () => {
 
 
   return (
-    <div>
-
+    
+    <div className="h-screen flex overflow-auto">
       <TableFormContainer />
-
+      <div className="m-4">
+        <div>
+          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Instruction</button>
+        </div>
+        <div>
+          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Research</button>
+        </div>
+        <div>
+          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Extension</button>
+        </div>
+        <div>
+          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Support Function</button>
+        </div>
+        <div>
+          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Administrative Function</button>
+        </div>
+        <div>
+          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Report Generation</button>
+        </div>
+      </div>
     </div>
   );
 };
