765df9c3b08d7eac3361eb08dfd594a7746b4e83
xcutiepatootie <mackycalabia@gmail.com>
2023-06-06 02:30:40 +0800

    dashboard

--- src/components/Dean/Dashboard.js
+++ src/components/Dean/Dashboard.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
-
 import TableFormContainer from './TableFormContainer';
 import { useSession } from 'next-auth/react';
+import Btndashboard from './Btndashboard';
 
-const Dashboard = () => {
+const Dashboard = ({  }) => {
   const [selectedCollection, setSelectedCollection] = useState(null);
+  const [selectedButton, setSelectedButton] = useState(null);
   const router = useRouter()
   const { data: session, status } = useSession()
 
@@ -29,29 +29,10 @@ const Dashboard = () => {
 
 
   return (
-    
+
     <div className="h-screen flex overflow-auto">
       <TableFormContainer />
-      <div className="m-4">
-        <div>
-          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Instruction</button>
-        </div>
-        <div>
-          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Research</button>
-        </div>
-        <div>
-          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Extension</button>
-        </div>
-        <div>
-          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Support Function</button>
-        </div>
-        <div>
-          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Administrative Function</button>
-        </div>
-        <div>
-          <button type="submit" className="ml-96 border-solid border-blue-950 border-2 text-blue-950 hover:bg-blue-950 hover:text-white font-bold h-28 w-28 rounded-[12px]">Report Generation</button>
-        </div>
-      </div>
+     
     </div>
   );
 };
